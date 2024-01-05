ec72a4b13a52161c1c479798dc77ef23282b5e44
Arnthorny <chidubem.anthony26@gmail.com>
2024-01-05 03:22:35 +0000

    Added return statement for Task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,10 @@
 export default function handleResponseFromAPI(promise) {
-  promise
+  return promise
     .then(
       () => ({ status: 200, body: 'success' }),
       () => new Error(),
     )
-    .then(() => {
+    .finally(() => {
       console.log('Got a response from the API');
     });
 }
