68f52481835d4d0bb99716282b4e76b520cd5690
Arnthorny <chidubem.anthony26@gmail.com>
2024-01-12 17:59:58 +0000

    Correction to tasks 8 and 11

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,9 @@
 export default function cleanSet(set, startString) {
+  if (!(set instanceof Set) || !(typeof startString === 'string')) return '';
   const newArr = Array.from(set)
-    .filter((x) => startString && x.startsWith(startString))
+    .filter(
+      (x) => startString && typeof x === 'string' && x.startsWith(startString),
+    )
     .map((x) => x.slice(startString.length));
   return newArr.join('-');
 }
