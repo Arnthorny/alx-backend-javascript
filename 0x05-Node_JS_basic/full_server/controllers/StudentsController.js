f00cdf930347837e7e29afaa6ff10c1462bd67db
Arnthorny <chidubem.anthony26@gmail.com>
2024-05-02 13:45:06 -0700

    Correction to final task

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,42 +1,43 @@
-import readDatabase from "../utils";
+import readDatabase from '../utils';
 
 const path = process.argv[2];
 
 class studentsController {
   static async getAllStudents(req, res) {
+    let retStr = 'This is the list of our students\n';
     await readDatabase(path).then(
       (dataObj) => {
         res.status(200);
-        let retStr = "This is the list of our students\n";
-
-        const keyArr = Object.keys(dataObj).sort((a, b) =>
-          a.toLowerCase() < b.toLowerCase() ? -1 : 1
-        );
+        const retArr = [];
+        const keyArr = Object.keys(dataObj).sort((a, b) => (
+          a.toLowerCase() < b.toLowerCase() ? -1 : 1));
 
         for (const field of keyArr) {
           if (Array.isArray(dataObj[field])) {
             const len = dataObj[field].length;
-            const firstnames = dataObj[field].join(", ");
-            retStr += `Number of students in ${field}: ${len}. List: ${firstnames}\n`;
+            const firstnames = dataObj[field].join(', ');
+            retArr.push(
+              `Number of students in ${field}: ${len}. List: ${firstnames}`,
+            );
           }
         }
-        res.send(retStr.trimRight());
+        res.send((retStr += retArr.join('\n')));
       },
       (err) => {
-        res.status(500).send(err.message);
-      }
+        res.status(500).send((retStr += err.message));
+      },
     );
   }
 
   static async getAllStudentsByMajor(req, res) {
-    const validMajors = ["CS", "SWE"];
+    const validMajors = ['CS', 'SWE'];
     const major = validMajors.filter((major) => major === req.params.major)[0];
     if (major === undefined) {
-      res.status(500).send("Major parameter must be CS or SWE");
+      res.status(500).send('Major parameter must be CS or SWE');
     } else {
       await readDatabase(path).then(
         (dataObj) => {
-          const firstnames = dataObj[major].join(", ");
+          const firstnames = dataObj[major].join(', ');
 
           res.status(200);
           res.send(`List: ${firstnames}`);
@@ -44,7 +45,7 @@ class studentsController {
         (err) => {
           res.status(500);
           res.send(err.message);
-        }
+        },
       );
     }
   }
--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,25 +1,27 @@
-import { promises as fs } from 'fs';
+import fs from 'fs';
 
-async function readDatabase(path) {
-  try {
-    const data = await fs.readFile(path, { encoding: 'utf-8' });
+function readDatabase(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, (error, data) => {
+      if (error) {
+        reject(Error('Cannot load the database'));
+      } else {
+        const strRows = String(data).split('\n');
+        const dataObj = {};
+        const actualRows = strRows.map((strRow) => strRow.split(','));
+        const dataRows = actualRows.slice(1).filter((row) => row.length === 4);
 
-    const strRows = data.split('\n');
-    const dataObj = {};
+        dataRows.forEach((row) => {
+          const field = row[3];
+          dataObj[field] = dataObj[field] === undefined ? [] : dataObj[field];
 
-    const actualRows = strRows.map((strRow) => strRow.split(','));
-    const dataRows = actualRows.slice(1).filter((row) => row.length === 4);
+          dataObj[field].push(row[0]);
+        });
 
-    dataRows.forEach((row) => {
-      const field = row[3];
-      dataObj[field] = dataObj[field] === undefined ? [] : dataObj[field];
-
-      dataObj[field].push(row[0]);
+        resolve(dataObj);
+      }
     });
-    return dataObj;
-  } catch (err) {
-    throw Error('Cannot load the database');
-  }
+  });
 }
 
 export default readDatabase;
