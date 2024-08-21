170f63f58bd990e12e0bd5081ad84efc4115335a
Arnthorny <chidubem.anthony26@gmail.com>
2024-08-21 12:01:57 -0700

    fix: Added missing test to api.test.js

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -6,6 +6,30 @@ const request = require("request");
 
 const API_URL = "http://localhost:7865";
 
+describe("Basic Integration testing", function () {
+  // Uncomment this code to allow tests run independent of server instance
+  // Start the server before running tests
+  //   before((done) => {
+  //     server = app.listen(7865, () => {
+  //       done();
+  //     });
+  //   });
+
+  //   // Stop the server after tests
+  //   after((done) => {
+  //     server.close(done);
+  //   });
+
+  it("should test 200 response value", function (done) {
+    request.get(`${API_URL}/`, (error, response, body) => {
+      if (error) return done(error);
+      expect(response.statusCode).to.eq(200);
+      expect(body).to.eq("Welcome to the payment system");
+      done();
+    });
+  });
+});
+
 describe("Regex integration testing", function () {
   it("should test 200 response value", function (done) {
     request.get(`${API_URL}/cart/12`, (error, response, body) => {
